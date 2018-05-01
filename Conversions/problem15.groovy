df85c39bef506515901630aa314511de55ab3143
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:06:45 +0800

    2018 Updates

---
+++ Conversions/problem15.groovy
@@ -0,0 +1,8 @@
+//Problem 15: Write a program that converts letters to ASCII.
+class Problem15{
+ static void main(String[]args){
+  char letter = 'x';
+  int ascii = (int)letter;
+  println(ascii);
+ }
+}
