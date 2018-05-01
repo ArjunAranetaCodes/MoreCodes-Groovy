6d61201f29fbae0546358353aafdf8d29dd672bf
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:07:23 +0800

    2018 Updates

---
+++ Conversions/problem18.groovy
@@ -0,0 +1,8 @@
+//Problem 18: Write a program that converts liter to milliliter.
+class Problem18{
+ static void main(String[]args){
+  int liter = 1;
+  int milliliter = liter * 1000;
+  println(milliliter);
+ }
+}
