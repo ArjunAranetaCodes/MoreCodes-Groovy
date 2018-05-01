cdb871a776fe5d8b010aab108f13bd0f3f6f5551
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-01 10:15:50 +0800

    2018 Updates

---
+++ Conversions/problem5.groovy
@@ -0,0 +1,8 @@
+//Problem 5: Write a program that converts an array/list to string.
+class Problem5{
+ static void main(String[]args){
+  String[] arrNumbers = ["1", "2", "3"];
+  String numbers = String.join("",arrNumbers);
+  println(numbers);
+ }
+}
