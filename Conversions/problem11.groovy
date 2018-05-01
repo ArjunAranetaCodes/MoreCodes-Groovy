3d982d303c4f332da7bba1b3bff3775d282e2433
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:05:53 +0800

    2018 Updates

---
+++ Conversions/problem11.groovy
@@ -0,0 +1,8 @@
+//Problem 11: Write a program that converts numbers of day to minutes.
+class Problem11{
+ static void main(String[]args){
+  int day = 1;
+  int minutes = day * 24 * 60;
+  println(minutes);
+ }
+}
