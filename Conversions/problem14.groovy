edf0284f222ac8531d1f8dfc9c2d1552cfce07d4
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:06:34 +0800

    2018 Updates

---
+++ Conversions/problem14.groovy
@@ -0,0 +1,7 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+class Problem14{
+ static void main(String[]args){
+  String str = "abc";
+  println(str.toUpperCase());
+ }
+}
