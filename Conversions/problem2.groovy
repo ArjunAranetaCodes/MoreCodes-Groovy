a420d128807485b85e49317fe960e59ba04e728d
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-01 10:14:33 +0800

    2018 Updates

---
+++ Conversions/problem2.groovy
@@ -0,0 +1,7 @@
+//Problem 2: Write a program that converts a string to integer.
+class Problem2{
+  static void main(String[]args){
+    String num = "10";
+    println(Integer.valueOf(num));
+  }
+}
