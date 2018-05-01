da11f3727a9a31dfd9c129cb53ca5a1e2dc6b889
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:07:12 +0800

    2018 Updates

---
+++ Conversions/problem17.groovy
@@ -0,0 +1,8 @@
+//Problem 17: Write a program that converts kilometer to meter.
+class Problem17{
+ static void main(String[]args){
+  int kilometer = 1;
+  int meter = kilometer * 1000;
+  println(meter);
+ }
+}
