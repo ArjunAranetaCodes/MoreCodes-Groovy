944aed05ce9cd33378547920eb62ece797ea7292
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-02 07:06:09 +0800

    2018 Updates

---
+++ Conversions/problem12.groovy
@@ -0,0 +1,8 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+class Problem12{
+ static void main(String[]args){
+  int day = 1;
+  int seconds = day * 24 * 60 * 60;
+  println(seconds);
+ }
+}
