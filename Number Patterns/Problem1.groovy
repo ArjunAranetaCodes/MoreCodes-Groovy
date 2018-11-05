0e09bc7890c0af15ebbc489656d58bb75cb839fc
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-11-05 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem1.groovy
@@ -0,0 +1,18 @@
+/*
+Problem 1: Write a program to print the number pattern of ones and zeros using loop.
+11111
+11111
+11111
+11111
+11111
+*/
+class Problem{
+ static void main(String[] args){
+  for(int y = 0; y < 5; y++){
+   for(int x = 0; x < 5; x++){
+    print("1");
+   }
+   println();
+  }
+ }
+}
